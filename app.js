4e75ec00c6895ef159c6d9aa176099b074c59545
Nicholas Celiano <nicholasceliano@gmail.com>
2017-01-14 12:48:21 -0700

    Added links to watch page

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 //C:\git\FamilyProject
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
 
 //Routes
 app.get('/', function (req, res) {
-  res.sendFile('home.html', {root: __dirname })
-})
+	res.sendFile('home.html', {root: __dirname });
+});
+
+app.get('/watch', function (req, res) {
+	var id = req.query.id;
+	
+	res.render('watch', { videoName: id, videoUrl: "https://s3.amazonaws.com/videos.celiano/" + id + ".mp4" });
+});
 
 //App Start
 app.listen(3000, function () {
-  console.log('App listening on port 3000')
-})
+	console.log('App listening on port 3000');
+});
