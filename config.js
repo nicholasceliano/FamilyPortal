aa911b6113131d3be455bdb33413fda15f6340db
Nicholas Celiano <nicholasceliano@gmail.com>
2017-02-08 11:13:11 -0700

    Set up Prod DB backup scheduled nightly at 12 AM MST. Created Dev database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	dev: {
-		db: 'mongodb://familyPortalUser:celiano@ec2-52-55-164-103.compute-1.amazonaws.com/familyportal',
+		db: 'mongodb://familyPortalUser_Dev:celiano_Dev@ec2-52-55-164-103.compute-1.amazonaws.com/familyportal_dev',
 		port: 3000,
 		videosBaseUri: 'https://s3.amazonaws.com/videos.celiano/'
 	},
