34337685a1e22608aaa00cba37ba966d464c25c1
Nicholas Celiano <nicholasceliano@gmail.com>
2017-02-13 11:30:09 -0700

    changed dev port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	dev: {
 		db: 'mongodb://familyPortalUser_Dev:celiano_Dev@ec2-52-55-164-103.compute-1.amazonaws.com/familyportal_dev',
-		port: 3000,
+		port: 3333,
 		videosBaseUri: 'https://s3.amazonaws.com/videos.celiano/'
 	},
 	
