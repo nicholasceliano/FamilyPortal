10286d87f16efdab0f0371596f1af7fffe240c7d
Nicholas Celiano <nicholasceliano@gmail.com>
2017-03-10 12:10:47 -0700

    Fixed custom scroll bar directive

--- content/app/shared/directives/scrollContainerDir.js
+++ content/app/shared/directives/scrollContainerDir.js
@@ -1,26 +1,36 @@
-familyPortalApp.directive('scrollContainer', function() {
-	return function (scope, element) {
-		scope.$watch('', function (n, o) {
-			var scrollContainer = $(element);
-			var scrollContent = $(element).find(">:first-child");
+familyPortalApp.directive('scrollRepeatContent', function($timeout) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			var ul = $(element).parent();
+			var scrollContent = ul.parent();
+			var panelBody = scrollContent.parent('.panel-body');
+			ul.hide();
 			
-			scrollContainer.addClass('scroll-container');
-			var offset = Math.round(getScrollBarWidth() + scrollContainer.width() - 5);
-			
-			scrollContent.width(offset).addClass('scroll-content');
-			
-			scrollContent.scroll(function(){
-				var scrollContent = $(this);
-				var content = $(this).children();
-							
-				if (content.height() > scrollContent.height())
-					recaulcuateScrollbarTop(this);
-			});
-			
-			buildScrollbar(scrollContainer);
-		});
-    };     
-	
+			if (scope.$last === true) {
+                $timeout(function () {
+					ul.show();
+					
+					scrollContent.wrap('<div class="scroll-container"></div>');
+					var scrollContainer = scrollContent.parent('.scroll-container');
+					scrollContent.addClass('scroll-content');
+					
+					if (ul.height() > panelBody.height()) {					
+						var offset = Math.round(getScrollBarWidth() + scrollContainer.width());
+						scrollContent.width(offset);
+						
+						$(window).resize(function (){
+							var offset = Math.round(getScrollBarWidth() + scrollContainer.width());
+							scrollContent.width(offset);
+						});
+						
+						buildScrollbar(scrollContainer, scrollContent);
+					}
+				});
+			}
+		}
+	};
+		
 	function getScrollBarWidth() {
 		var outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
 			widthWithScroll = $('<div>').css({width: '100%'}).appendTo(outer).outerWidth();
@@ -28,13 +38,67 @@ familyPortalApp.directive('scrollContainer', function() {
 		return 100 - widthWithScroll;
 	}
 	
-	function buildScrollbar(scrollContainer) {
+	function buildScrollbar(scrollContainer, scrollContent) {
 		var scrollbarContainerHeight = scrollContainer.height();
 		var scrollbarContainer = $('<div class="scrollbar-container"></div>').css({top: -scrollbarContainerHeight});
 		var scrollbar = $('<div class="scrollbar"></div>').css({top: 0});
+		var manualScroll;
+		var mouseDown = false;
 		
 		scrollContainer.append(scrollbarContainer);
 		scrollbarContainer.append(scrollbar);
+		
+		scrollContent.scroll(function(){
+			if (!mouseDown){
+				var scrollContent = $(this);
+				var content = $(this).children();
+							
+				if (content.height() > scrollContent.height())
+					recaulcuateScrollbarTop(this);
+			}
+		});
+		
+		scrollbar.mousedown(function(event) {			
+			mouseDown = true;
+			var mousePosition = event.clientY;
+			scrollbar.addClass('scrollbar-selected');
+			$('body').addClass('noselect');
+			
+			$(document).mousemove(function(event) {
+				mousePosition = event.clientY;
+			});
+			
+			 manualScroll = setInterval(function () {
+				var scrollbarContainterTop = scrollContainer.offset().top;
+				var scrollbarContainterBottom = scrollContainer.offset().top + scrollContainer.height();
+				var scrollbarPosition = scrollbarContainterTop;
+				
+				if (mousePosition < (scrollbarContainterTop))
+					scrollbarPosition = scrollbarContainterTop;
+				else if (mousePosition > (scrollbarContainterBottom - scrollbar.height()))
+					scrollbarPosition = scrollbarContainterBottom - scrollbar.height();
+				else 
+					scrollbarPosition = mousePosition;
+				
+				scrollbarPosition = scrollbarPosition - scrollbarContainterTop;
+				
+				var percentScrolled = (scrollbarPosition / scrollContainer.height());
+				var maxScrollHeight = scrollContent[0].scrollHeight - scrollContent.height()
+				var scrollbarTop = percentScrolled * maxScrollHeight;
+				
+				scrollContent.scrollTop(scrollbarTop);
+								
+				scrollbar.css({top: scrollbarPosition});
+			});
+			
+		});
+				
+		$(window).mouseup(function() {
+			mouseDown = false;
+			scrollbar.removeClass('scrollbar-selected');
+			$('body').removeClass('noselect');
+			clearInterval(manualScroll);
+		});
 	}
 	
 	function recaulcuateScrollbarTop(scrollContent) {
@@ -42,7 +106,7 @@ familyPortalApp.directive('scrollContainer', function() {
 		var scrollbar = $(element).find('.scrollbar');
 		
 		var percentScrolled = scrollContent.scrollTop / (scrollContent.scrollHeight - $(scrollContent).height());
-		var maxScrollHeight = $(scrollContent).height() - 20;//-20 for scrollbar height
+		var maxScrollHeight = $(scrollContent).height() - scrollbar.height();
 		var scrollbarTop = percentScrolled * maxScrollHeight;
 		
 		scrollbar.show();
